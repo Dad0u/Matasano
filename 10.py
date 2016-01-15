c8325cccb39810e92680e69cda2473f6192f9b23
Dad0u <victor.couty@gmail.com>
2016-01-15 15:57:26 +0100

    Ajout de 10.py

---
+++ 10.py
@@ -0,0 +1,42 @@
+from Crypto.Cipher import AES
+from Tools import xor
+
+class AES_CBC():
+  def __init__(self, key):
+    self.cipher = AES.AESCipher(key,AES.MODE_ECB)
+    self.iv = bytes(16)
+
+  def encrypt(self, data):
+    enc = self.iv
+    out = b''
+    if len(data)%16 != 0:
+      data += bytes(16-len(data)%16)
+    for i in range(len(data)//16):
+      block = data[16*i:16*(i+1)]
+      block = xor(block,enc)
+      enc = self.cipher.encrypt(block)
+      out += enc
+    if len(data)%16 == 0:
+      return out
+      
+
+  def decrypt(self, data):
+    out = b''
+    enc = bytes(len(data))
+    prev = self.iv
+    for i in range(len(data)//16):
+      block = data[16*i:16*(i+1)]
+      dec = xor(self.cipher.decrypt(block),prev)
+      out += dec
+      prev = block
+    return out.rstrip(bytes(1))
+
+data = b''
+with open('10.txt', 'r') as f:
+  data = bytes(f.read(),'utf-8')
+
+cbc = AES_CBC('YELLOW SUBMARINE')
+
+enc = cbc.encrypt(data)
+
+print(cbc.decrypt(enc))
