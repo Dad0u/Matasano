251cab29a84fcdbf16e57a092d88bbcb59f190ea
Dad0u <victor.couty@gmail.com>
2016-01-21 18:44:23 +0100

    Test avec le fichier fourni

--- 10.py
+++ 10.py
@@ -1,41 +1,48 @@
 from Crypto.Cipher import AES
-from Tools import xor
+from Tools import xor,pkcs7_pad,pkcs7_unpad
+from Base64 import bytesFromBase64
 
 class AES_CBC():
-  def __init__(self, key):
+
+  def __init__(self, key, iv = bytes(16)):
     self.cipher = AES.AESCipher(key,AES.MODE_ECB)
-    self.iv = bytes(16)
+    self.iv = iv
 
   def encrypt(self, data):
     enc = self.iv
     out = b''
-    if len(data)%16 != 0:
-      data += bytes(16-len(data)%16)
+    data = pkcs7_pad(data)
     for i in range(len(data)//16):
       block = data[16*i:16*(i+1)]
       block = xor(block,enc)
       enc = self.cipher.encrypt(block)
       out += enc
-    if len(data)%16 == 0:
-      return out
+    return out
 
   def decrypt(self, data):
     out = b''
     enc = bytes(len(data))
+    #prev = bytes(16)
     prev = self.iv
     for i in range(len(data)//16):
       block = data[16*i:16*(i+1)]
       dec = xor(self.cipher.decrypt(block),prev)
       out += dec
       prev = block
-    return out.rstrip(bytes(1))
+    return pkcs7_unpad(out)
 
+"""
 data = b''
-with open('10.txt', 'r') as f:
+with open('10a.txt', 'r') as f:
   data = bytes(f.read().strip('\n'),"utf-8")
+"""
 
+with open('10.txt','r') as f:
+  data = bytesFromBase64("".join(f.read().split('\n')))
 cbc = AES_CBC('YELLOW SUBMARINE')
+#cbc2 = AES_CBC('YELLOW SUBMARINE', b'abcdefghijklmnop')
 
-enc = cbc.encrypt(data)
+print(cbc.decrypt(data))
 
-print(cbc.decrypt(enc))
+
+
