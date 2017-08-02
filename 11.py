6a3811cf531c410032a702b7e48fdd765312e620
Victor Couty <victor@couty.eu>
2017-08-02 19:41:07 +0200

    Rewritten 11

--- 11.py
+++ 11.py
@@ -1,56 +1,30 @@
 from Crypto.Cipher import AES
-from Tools import xor,pkcs7_pad
-from Base64 import bytesFromBase64
+from Tools import random_bytes,pkcs7_pad
 from random import randint
 
 class RandomEncryption():
-
-  def __init__(self, data):
-    self.data = data
-
-  def encryptCBC(self, data, key, iv):
-    enc = iv
-    out = b''
-    cipher = AES.AESCipher(key,AES.MODE_ECB)
-    for i in range(len(data)//16):
-      block = data[16*i:16*(i+1)]
-      block = xor(block,enc)
-      enc = cipher.encrypt(block)
-      out += enc
-    return out
-
-  def encryptECB(self, data, key):
-    cipher = AES.AESCipher(key,AES.MODE_ECB)
-    return cipher.encrypt(data)
-
-  def genblock(self):
-    data = pkcs7_pad(self.randBytes(randint(5,10)) + self.data + self.randBytes(randint(5,10)))
-    print(len(data))
-    c = randint(1,2)
-    if c == 1:
-      print('CBC')
-      return self.encryptCBC(data,self.randBytes(),self.randBytes())
+  def __init__(self):
+    self.mode = "ECB" if randint(0,1) else "CBC"
+    if self.mode == "CBC":
+      args = [random_bytes(16),AES.MODE_CBC,random_bytes(16)]
     else:
-      print('ECB')
-      return self.encryptECB(data,self.randBytes())
+      args = [random_bytes(16),AES.MODE_ECB]
+    self.cipher = AES.AESCipher(*args)
 
-  def randBytes(self, n = 16):
-    out = b''
-    for i in range(n):
-      out += bytes([randint(0,255)])
-    return out
+  def encrypt(self,data):
+    data = random_bytes(randint(5,10))+data+random_bytes(randint(5,10))
+    return self.cipher.encrypt(pkcs7_pad(data))
 
-def detectEncryption(data):
-  if data[16:32] == data[32:48]:
-    print('ECB')
+def is_ecb(cipher,bs=16):
+  data = cipher.encrypt(bytes([97]*(bs*3)))
+  blocks = [data[i:i+bs] for i in range(0,len(data),bs)]
+  return len(set(blocks)) != len(blocks)
+
+for i in range(50):
+  c = RandomEncryption()
+  if is_ecb(c):
+    assert c.mode == "ECB"
   else:
-    print('CBC')
+    assert c.mode == "CBC"
 
-with open('10a.txt') as f:
-  data = bytes(f.read(),'utf-8')
-#data = bytes(64)
-
-oracle = RandomEncryption(data)
-block = oracle.genblock()
-print(block)
-detectEncryption(block)
+print("Success!")
